69184548fcc899c7e35ff2766366372c1100d828
morita2232 <sebastian.mora@enti.cat>
2025-05-16 19:23:50 +0200

    Un poco de avamce con cervezas

--- AA1_Plataformas_2D/Assets/Scripts/ChestLogic.cs
+++ AA1_Plataformas_2D/Assets/Scripts/ChestLogic.cs
@@ -5,6 +5,7 @@ public class ChestLogic : MonoBehaviour
 {
     public Animator chestAnimator;
     public GameObject ingredient;
+    public GameObject chest;
 
     void Start()
     {
@@ -24,7 +25,7 @@ public class ChestLogic : MonoBehaviour
         yield return new WaitForSeconds(2f);
 
         ingredient.SetActive(true);
-        ingredient.transform.position = new Vector3(14, 0, 0);
+        ingredient.transform.position = new Vector3(chest.transform.position.x, (chest.transform.position.y + 1), 0);
     }
 }
 
